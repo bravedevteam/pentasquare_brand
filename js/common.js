92f1efdd5c867b9bf0da8f97e573876cc7b09538
TeamBrave_Dev <73458930+bravedevteam@users.noreply.github.com>
2022-01-29 00:08:05 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,48 @@
+$(function(){
+  //Gnb
+  $("#btnMenu").click(function(){
+    if($(this).hasClass("close")){
+      $(this).removeClass("close");
+      $(".boxSelect").show();
+      $("#gnb").removeClass("is--open");
+    }else{
+      $(this).addClass("close");
+      $(".boxSelect").hide();
+      $("#gnb").addClass("is--open");
+    }
+  });
+
+
+  //Select
+  $(".boxSelect .list").slideUp(0);
+  $(".boxSelect .selected").click(function(e){
+    e.preventDefault();
+    if($(this).hasClass("on")){
+      $(this).removeClass("on");
+      $(this).next(".list").slideUp(300);  
+    }else{
+      $(this).addClass("on");
+      $(this).next(".list").slideDown(300);
+    }
+  });
+  $(".boxSelect .list li").click(function(){
+    var target = $(this).html();
+    $(".boxSelect .selected").html(target).removeClass("on");
+    $(".boxSelect .list").slideUp(300);
+  });
+
+
+  //Service
+  $(".boxService dd").slideUp(0);
+  $(".boxService dt:eq(0)").addClass("on");
+  $(".boxService dd:eq(0)").slideDown(300);
+  $(".boxServiceImg div:eq(0)").fadeIn(300);
+
+  $(".boxService dt").click(function(){
+    var idx = $(this).attr("data-role");
+    $(this).addClass("on").siblings("dt").removeClass("on")
+    $(this).next("dd").slideDown(300).siblings("dd").slideUp(300);
+    $(".boxServiceImg div:eq("+idx+")").fadeIn(300).siblings().fadeOut(300);
+  });
+
+});
